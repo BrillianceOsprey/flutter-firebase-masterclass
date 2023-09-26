6130e0ebc5fd36d964d1d5a8f5348bbda2cf95fe
Andrea Bizzotto <codewithandrea@gmail.com>
2023-09-26 08:44:12 +0100

    Run "dart run build_runner build -d"

--- ecommerce_app/lib/src/features/products_admin/data/template_products_providers.g.dart
+++ ecommerce_app/lib/src/features/products_admin/data/template_products_providers.g.dart
@@ -193,4 +193,4 @@ class _TemplateProductProviderElement
   String get id => (origin as TemplateProductProvider).id;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
